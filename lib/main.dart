316d9eb5ef4919b4b6cee4ab2f153432f969307b
dtlucho <luciano.ditomaso@gmail.com>
2022-01-31 09:35:25 -0300

    adding routes + initial route

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_products/screens/screens.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      title: 'Products App',
+      initialRoute: "login",
+      routes: {
+        "login": (_) => const LoginScreen(),
+        "home": (_) => const HomeScreen(),
+      },
     );
   }
 }
