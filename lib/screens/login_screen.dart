9704e8b167ab970af3f0be53ba314817d1386dca
dtlucho <luciano.ditomaso@gmail.com>
2022-01-31 12:24:52 -0300

    form layout + input style

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -18,12 +18,59 @@ class LoginScreen extends StatelessWidget {
                     const SizedBox(height: 10),
                     Text("Login", style: Theme.of(context).textTheme.headline4),
                     const SizedBox(height: 30),
-                    const Text("Form"),
+                    _LoginForm(),
                   ],
                 ),
               ),
+              const SizedBox(height: 50.0),
+              const Text(
+                "Create a new account",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class _LoginForm extends StatelessWidget {
+  const _LoginForm({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Form(
+        child: Column(
+          children: [
+            TextFormField(
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.deepPurple,
+                  ),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.deepPurple,
+                    width: 2,
+                  ),
+                ),
+                hintText: "john.doe@email.com",
+                labelText: "Email",
+                labelStyle: TextStyle(
+                  color: Colors.grey,
+                ),
+                prefixIcon: Icon(
+                  Icons.alternate_email_sharp,
+                  color: Colors.deepPurple,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
