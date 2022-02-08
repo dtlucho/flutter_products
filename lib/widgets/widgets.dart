f42ee9864f5eb9a0dd2e3784a14a4db4fc1b82ab
dtlucho <luciano.ditomaso@gmail.com>
2022-02-07 21:07:06 -0300

    exporting widget

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,2 +1,3 @@
 export 'package:flutter_products/widgets/auth_background.dart';
 export 'package:flutter_products/widgets/card_container.dart';
+export 'package:flutter_products/widgets/product_card.dart';
