67e2d0e94f96b94f476a311d97f14a065a8d566a
dtlucho <luciano.ditomaso@gmail.com>
2022-02-07 21:07:39 -0300

    home screen main layout

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_products/widgets/product_card.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("HomeScreen"),
+    return Scaffold(
+      appBar: AppBar(title: const Text('Products')),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (_, int index) => const ProductCard(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add, color: Colors.white),
+        onPressed: () {},
       ),
     );
   }
